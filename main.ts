1b319f8481b210f07b8e5517d0ff5793e6f85110
David Schiavone <david.schiavone@hftm.ch>
2025-01-15 20:19:59 +0100

    feat(skywalker-api): implement methods in main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import fetch, { Response } from "node-fetch";
-import { map, mergeMap } from "rxjs/operators";
-import { get } from "./utils";
-import { of } from "rxjs";
+import fetch, {Response} from "node-fetch";
+import {map, mergeMap} from "rxjs/operators";
+import {get} from "./utils";
+import {forkJoin} from "rxjs";
 
 /* 
 Read data from https://swapi.dev/api/people/1 (Luke Skywalker)
@@ -26,6 +26,12 @@ Define an interface of the result type above and all other types as well.
 
 */
 
+interface Film {
+  title: string;
+  director: string;
+  release_date: string;
+}
+
 interface Person {
   name: string;
   height: string;
@@ -35,7 +41,11 @@ interface Person {
 }
 
 export interface PersonInfo {
-  // TODO: define type
+  name: string;
+  height: string;
+  gender: "male" | "female" | "divers";
+  homeworld: string;
+  films: Film[];
 }
 
 // Task 1: write a function using promise based fetch api
@@ -43,8 +53,25 @@ type PromiseBasedFunction = () => Promise<PersonInfo>;
 export const getLukeSkywalkerInfo: PromiseBasedFunction = () => {
   return fetch("https://swapi.dev/api/people/1").then((response: Response) => {
     return response.json().then((person: Person) => {
-      // TODO: load other stuff and return LukeSkywalkerInfo
-      return {} as PersonInfo;
+      const homeworldPromise = fetch(person.homeworld).then( resp => resp.json());
+
+      const filmPromises = Promise.all(person.films.map((filmUrl) =>
+          fetch(filmUrl).then((response) => response.json())
+      ));
+
+      return Promise.all([homeworldPromise, filmPromises]).then(([homeworld, films]) =>{
+        return {
+        name: person.name,
+        height: person.height,
+        gender: person.gender,
+        homeworld: homeworld.name,
+        films: films.map((film) => ({
+          title: film.title,
+          director: film.director,
+          release_date: film.release_date,
+        })),
+      };
+    });
     });
   });
 };
@@ -54,17 +81,52 @@ export const getLukeSkywalkerInfo: PromiseBasedFunction = () => {
 type AsyncBasedFunction = () => Promise<PersonInfo>;
 export const getLukeSkywalkerInfoAsync: PromiseBasedFunction = async () => {
   const response = await fetch("https://swapi.dev/api/people/1");
-  // TODO: load other stuff and return LukeSkywalkerInfo
-  return (await {}) as PersonInfo;
+  const person: Person = await response.json();
+
+  const homeworldResponse = await fetch(person.homeworld);
+  const homeworldData = await homeworldResponse.json();
+
+  const filmPromises = person.films.map((filmUrl) =>
+      fetch(filmUrl).then((response) => response.json())
+  );
+  const films = await Promise.all(filmPromises);
+
+  return {
+    name: person.name,
+    height: person.height,
+    gender: person.gender,
+    homeworld: homeworldData.name,
+    films: films.map((film) => ({
+      title: film.title,
+      director: film.director,
+      release_date: film.release_date,
+    })),
+  };
 };
 
 // Task 3: write a function using Observable based api
 // see also: https://rxjs.dev/api/index/function/forkJoin
 export const getLukeSkywalkerInfoObservable = () => {
   return get<Person>("https://swapi.dev/api/people/1").pipe(
-    mergeMap((person: Person) => {
-      // TODO: load other stuff and return LukeSkywalkerInfo
-      return of({} as PersonInfo);
-    })
+      mergeMap((person: Person) => {
+        const homeworld$ = get<{ name: string }>(person.homeworld);
+        const films$ = forkJoin(
+            person.films.map((filmUrl) => get<{ title: string; director: string; release_date: string }>(filmUrl))
+        );
+
+        return forkJoin({ homeworld: homeworld$, films: films$ }).pipe(
+            map(({ homeworld, films }) => ({
+              name: person.name,
+              height: person.height,
+              gender: person.gender,
+              homeworld: homeworld.name,
+              films: films.map((film) => ({
+                title: film.title,
+                director: film.director,
+                release_date: film.release_date,
+              })),
+            }))
+        );
+      })
   );
 };
